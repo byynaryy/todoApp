0063fad62a5c1a96568690dfeaad8197072ab780
byynaryy <endeki@gmail.com>
2020-10-01 06:27:30 +0200

    Add TodoApp component

---
+++ src/components/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class TodoApp extends React.Component {
+    render() {
+        return(
+            <h1>Todos</h1>
+        );
+    }
+}
+
+export default TodoApp;
